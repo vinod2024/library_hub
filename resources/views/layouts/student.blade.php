4b90d699c727ddcfaa625d669a00da239ee5df0f
vinod <vinod4web@gmail.com>
2025-07-05 23:54:18 +0530

    student dashboard show after login

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -66,15 +66,7 @@
             </button>
             <div class="collapse navbar-collapse" id="studentNavbar">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ route('student.dashboard') }}">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ route('student.profile') }}">Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ route('student.join.form') }}">Join Library</a>
-                    </li>
+                    
                     <li class="nav-item">
                         <form action="{{ route('logout') }}" method="POST" class="d-inline">
                             @csrf
