30cedce2d040cea431b60843be9fc6fe42256bcc
vinod2024 <vinod4web@gmail.com>
2025-07-14 23:12:52 +0530

    fixed login and ragister issue

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return redirect()->route('login');
-
+    // return redirect()->route('login');
+    return redirect()->route('student.dashboard');
 });
 
 /* Route::get('/dashboard', function () {
