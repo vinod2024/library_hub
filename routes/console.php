9880f1d07d971873124c747021b20f79956ed463
vinod2024 <vinod4web@gmail.com>
2025-07-15 01:41:10 +0530

    modify free seat scheduler

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('free:seat')->dailyAt('01:20');
+Schedule::command('free:seat')->dailyAt('01:45');
